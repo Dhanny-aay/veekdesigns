1f5bf3506c6b323cad419d0f2b4aafd95a0b59d6
Dhanny-aay <danielakintolu@gmail.com>
2023-07-30 23:18:52 +0100

    worked on projext component

--- src/app/comps/projects.js
+++ src/app/comps/projects.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import proone from '../images/proone.svg';
+import protwo from '../images/protwo.svg';
+import prothree from '../images/prothree.svg';
 const Projects = () => {
     return ( 
         <>
@@ -13,7 +15,7 @@ const Projects = () => {
                         <button className=" absolute top-20 md:top-4 right-6 font-medium text-xs lg:text-xl bg-[#00000036] backdrop-blur-[5px] py-3 px-4 text-white font-Archivo rounded-[17px]">Swipe for more</button>
                     </div>
                 </div>
-                <div className="w-full md:w-[48%] space-y-3 lg:space-y-5 flex flex-col">
+                <div className="w-full md:w-[48%] space-y-3 lg:space-y-9 flex flex-col">
                     <p className=" font-Archivo text-base lg:text-[20px] font-medium">Project Name:<span className=" text-sm lg:text-base font-extralight">  Chawcad</span></p>
                     <span className="">
                         <p className=" font-Archivo text-base lg:text-[20px]  font-medium">Services Provided</p>
@@ -29,8 +31,8 @@ const Projects = () => {
                 </div>
             </div>
 
-            <div className=" flex flex-col md:flex-row justify-around items-center w-full mt-6 md:mt-8 lg:mt-[64px]">
-                <div className="w-full md:w-[48%] space-y-3 lg:space-y-5 flex flex-col">
+            <div className=" flex flex-col-reverse md:flex-row justify-around items-center w-full mt-6 md:mt-8 lg:mt-[64px]">
+                <div className="w-full md:w-[48%] space-y-3 lg:space-y-9 flex flex-col">
                     <p className=" font-Archivo text-base lg:text-[20px] font-medium">Project Name:<span className=" text-sm lg:text-base font-extralight">  Chawcad</span></p>
                     <span className="">
                         <p className=" font-Archivo text-base lg:text-[20px]  font-medium">Services Provided</p>
@@ -46,9 +48,32 @@ const Projects = () => {
                 </div>
                 <div className=" w-full md:w-[48%] md:flex justify-between">
                     <div className=" lg:w-[500px] w-full md:w-[340px] h-[400px] md:h-[280px] lg:h-[400px] bg-transparent relative">
-                        <Image src={ proone } className=" w-[100%] rounded-[50px] h-full" alt="" />
+                        <Image src={ protwo } className=" w-[100%] rounded-[50px] h-full" alt="" />
                         <button className=" absolute top-20 md:top-4 right-6 font-medium text-xs lg:text-xl bg-[#00000036] backdrop-blur-[5px] py-3 px-4 text-white font-Archivo rounded-[17px]">Swipe for more</button>
                     </div>
+                </div>
+            </div>
+
+            <div className=" flex flex-col md:flex-row justify-around items-center w-full mt-6 md:mt-8 lg:mt-[64px]">
+                <div className=" w-full md:w-[48%] md:flex justify-between">
+                    <div className=" lg:w-[500px] w-full md:w-[340px] h-[400px] md:h-[280px] lg:h-[400px] bg-transparent relative">
+                        <Image src={ prothree } className=" w-[100%] rounded-[50px] h-full" alt="" />
+                        <button className=" absolute top-20 md:top-4 right-6 font-medium text-xs lg:text-xl bg-[#00000036] backdrop-blur-[5px] py-3 px-4 text-white font-Archivo rounded-[17px]">Swipe for more</button>
+                    </div>
+                </div>
+                <div className="w-full md:w-[48%] space-y-3 lg:space-y-9 flex flex-col">
+                    <p className=" font-Archivo text-base lg:text-[20px] font-medium">Project Name:<span className=" text-sm lg:text-base font-extralight">  Weather app</span></p>
+                    <span className="">
+                        <p className=" font-Archivo text-base lg:text-[20px]  font-medium">Services Provided</p>
+                        <p className=" font-Archivo text-sm lg:text-base font-light">UX Design, UI Design, Visual Design, Prototyping</p>
+                    </span>
+                    <span className="">
+                        <p className=" font-Archivo text-base lg:text-[20px]  font-medium">Description</p>
+                        <p className=" font-Archivo text-sm lg:text-base font-light">The Chawcad app is your ultimate dining companion, offering a seamless and convenient payment experience. Scan, split, and pay instantly with ease, whether you're online or offline. Trust in Chawcad's secure and reliable app to elevate your dining experience.</p>
+                    </span>
+                    <a className=" w-full h-[45px] lg:h-[70px] bg-[#f5f5f5] flex justify-between items-center px-6 rounded-[50px]">
+                            <p className=" font-Archivo text-sm lg:text-xl font-normal">View Project</p>
+                    </a>
                 </div>
             </div>
         </div>
